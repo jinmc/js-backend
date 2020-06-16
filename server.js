d2dfc9213fe3c73cbe81ad7d4824fe2f3aba9e0d
Jim Chong <jinmo55555@gmail.com>
2020-06-16 11:23:36 -0400

    add mongo

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 'use strict'
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -11,6 +15,14 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoogse'))
 
 app.use('/', indexRouter)
 

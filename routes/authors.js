6aca962713c2e4bd55c6dc29e787aeffea2b6fe9
Jim Chong <jinmo55555@gmail.com>
2020-06-24 18:10:28 -0400

    add filter in authors page using regexp

--- routes/authors.js
+++ routes/authors.js
@@ -4,7 +4,20 @@ const Author = require('../models/author')
 
 // All Authors Route
 router.get('/', async (req, res) => {
-    res.render('authors/index')
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    } 
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+
 })
 
 // New Author Route

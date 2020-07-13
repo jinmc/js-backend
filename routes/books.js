d64f5bfe99333f3773e7aaaf8b9b911abd849606
Jim Chong <jinmo55555@gmail.com>
2020-07-13 14:27:28 +0900

    add book routes and logic

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ const express = require('express')
 const multer = require('multer')
 const router = express.Router()
 const path = require('path')
+const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
 const uploadPath = path.join('public', Book.coverImageBasePath)
@@ -15,7 +16,25 @@ const upload = multer({
 
 // All Books Route
 router.get('/', async (req, res) => {
-    res.send('All books')
+    let query = Book.find()
+    if (req.query.title != null && req.query.title !== '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try {
+        const books = await query.exec()
+        res.render('books/index', {
+            books: books,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New Book Route
@@ -38,9 +57,18 @@ router.post('/', upload.single('cover'), async (req, res) => {
         const newBook = await book.save()
         res.redirect('books')
     } catch {
+        if (book.coverImageName != null) {
+            removeBookCover(book.coverImageName)
+        }
         renderNewPage(res, book, true)
     }
 })
+
+function removeBookCover(fileName) {
+    fs.unlink(path.join(uploadPath, fileName), error => {
+        if (error) console.error(err)
+    })
+}
 
 async function renderNewPage(res, book, hasError = false) {
     try {
